b7448cf2a526991bde2fa2d22b55e7b54cf32049
Alexandr Lagodich <sagodich@mail.ru>
2021-07-06 18:43:09 +0300

    lesson3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 //...........................................Lesson№1..................................................
 
 // const btns = document.querySelectorAll('button'),
@@ -38,3 +37,46 @@
 // const btn = document.createElement('button');
 // btn.classList.add('red');
 // wrapper.append(btn);
+
+//...........................................Lesson№3..................................................
+
+// const btn = document.querySelector('.btn');
+// let timerId,
+//    i = 0;
+
+// function myAnimation() {
+//    const elem = document.querySelector('.box');
+//    let pos = 0;
+//    const id = setInterval(frame, 10);
+
+//    function frame() {
+//       if (pos == 300) {
+//          clearInterval(id);
+//       } else {
+//          pos++;
+//          elem.style.top = pos + "px";
+//          elem.style.left = pos + "px";
+//       }
+//    }
+// }
+
+// btn.addEventListener('click', myAnimation);
+
+// btn.addEventListener('click', () => {
+//    timerId = setInterval(logger, 1000);
+//    console.log('done');
+// });
+
+
+// function logger() {
+//    if (i==3) {
+//       clearInterval(timerId);
+//    }
+//    console.log('text');
+//    i++;
+// }
+
+// let id = setTimeout(function log(){
+//    console.log('htllo');
+//    id =  setTimeout( log, 500);
+// }, 500);
