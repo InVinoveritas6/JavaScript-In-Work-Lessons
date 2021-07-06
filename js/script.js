012d9364a2d8fe44aaab540b3c6995ec6573e8e4
Alexandr Lagodich <sagodich@mail.ru>
2021-07-06 14:02:43 +0300

    Lesson1

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+//...........................................Lesson№1..................................................
+
+// const btns = document.querySelectorAll('button'),
+//       wrapper = document.querySelector(".btn-block")
+// ;
+
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0));
+//  console.log(btns[1].classList.add('red'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
+
+// if (btns[1].classList.contains('red')) {
+//    console.log('hello');
+// }
+// btns[0].addEventListener('mouseenter', () =>{
+//    if(btns[1].classList.toggle('red')) {
+
+//    }
+// });
+
+// wrapper.addEventListener('click', (event) => {
+//    // console.dir(event.target);
+//       if (event.target && event.target.matches("button")) {
+//             console.log('hello');
+//       }
+// });
+
+// // btns.forEach(btn => {
+// //    btn.addEventListener('click', () => {
+// //       console.log('hello');
+// //    });
+// // });
+
+
+
+// const btn = document.createElement('button');
+// btn.classList.add('red');
+// wrapper.append(btn);
