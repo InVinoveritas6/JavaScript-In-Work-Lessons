c9156087efc764a889e643f38c51a712d4146205
Alexandr Lagodich <sagodich@mail.ru>
2021-09-02 11:49:55 +0300

    Lesson№9

--- js/script.js
+++ js/script.js
@@ -102,3 +102,29 @@
 // console.log(end - start);
 
 // alert(`${end-start}`);
+
+//...........................................Lesson№9..................................................
+
+// function User(name, id) {
+//    this.name = name;
+//    this.id = id;
+//    this.human = true;
+//    this.hello = function (){
+//       console.log(`Hello ${this.name}, you are ${this.id} y.o.`);
+//    };
+// }
+
+// User.prototype.exit = function() {
+//    console.log(`Пользователь ${this.name} ушел`);
+// };
+
+// const ivan = new User('Ivan', 23);
+// const alex = new User('Alex', 30);
+
+// ivan.exit();
+
+// ivan.hello();
+// alex.hello();
+
+// console.log(ivan);
+// console.log(alex);
