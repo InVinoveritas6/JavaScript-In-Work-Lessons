336f72b5265347b05d495d31b0ffccdccb9f350b
Alexandr Lagodich <sagodich@mail.ru>
2021-10-08 14:13:51 +0300

    Lesson11

--- js/script.js
+++ js/script.js
@@ -202,3 +202,38 @@
 // };
 
 //...........................................Lesson№11.................................................
+
+// class Rectangle {
+//    constructor (height, width) {
+//       this.height = height;
+//       this.width = width;
+//    }
+
+//    calcArea() {
+//       return this.height * this.width;
+//    }
+// }
+
+// class ColoredRectangleWithText extends Rectangle {
+//    constructor(height, width, text, bgColor) {
+//       super(height, width);
+//       this.text = text;
+//       this.bgColor = bgColor;
+//    }
+
+//    showMyProps() {
+//       console.log(`Текст ${this.text}, цветж ${this.bgColor}`);
+//    }
+// }
+
+// const div = new ColoredRectangleWithText(25, 10, 'Hello world', 'red');
+
+// div.showMyProps();
+// console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(10, 100);
+// console.log(square.calcArea());
+// console.log(long.calcArea());
+
+//...........................................Lesson№12.................................................
